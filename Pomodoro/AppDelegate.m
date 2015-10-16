f6a7312cd57c79973cfeaa9f6510fcc9d5dc6268
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-10-16 14:00:49 -0400

    Add PreferencesConfig

--- Pomodoro/AppDelegate.m
+++ Pomodoro/AppDelegate.m
@@ -14,6 +14,36 @@
 @end
 
 
+@interface PreferencesConfig : NSObject <Config>
+@end
+
+@implementation PreferencesConfig
+@dynamic restMinutes;
+@dynamic workMinutes;
+
++ (void)initialize
+{
+    [[NSUserDefaults standardUserDefaults] registerDefaults:
+        @{
+          @"WorkMinutes": @50,
+          @"RestMinutes": @10,
+          }];
+}
+
+
+- (NSTimeInterval)workMinutes
+{
+    return [[NSUserDefaults standardUserDefaults] doubleForKey:@"WorkMinutes"];
+}
+
+
+- (NSTimeInterval)restMinutes
+{
+    return [[NSUserDefaults standardUserDefaults] doubleForKey:@"RestMinutes"];
+}
+@end
+
+
 @interface PlistConfig : NSObject <Config>
 - (instancetype)initWithURL:(NSURL *)fileURL NS_DESIGNATED_INITIALIZER;
 @end
